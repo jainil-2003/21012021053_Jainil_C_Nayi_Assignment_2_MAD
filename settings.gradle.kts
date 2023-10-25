a158f964310f62e2b4c6d559dbc62d5bcc93a11a
jainil-2003 <nayijainilofficial@gmail.com>
2023-10-25 21:07:36 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "21012021053_Jainil_C_Nayi_Assignment_2_MAD"
+include(":app")
